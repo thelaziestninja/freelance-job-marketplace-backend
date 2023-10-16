7b3938c5e09e9328919711f8a3801ac7b770af16
Godji Berry <verginia.razvigorova@gmail.com>
2023-10-16 14:58:40 +0300

    Without comments

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-// import { IProfile } from "../types/profile";
 
 const profileSchema = new mongoose.Schema({
   skills: {
@@ -18,17 +17,6 @@ const profileSchema = new mongoose.Schema({
     type: [String],
   },
 });
-
-//Ex:
-// export interface ProfileDocument extends IProfile, Document {
-//     // Define any instance methods here
-//   }
-
-// Define any instance methods here, for example:
-// profileSchema.methods.getSkills = function(): string[] {
-//     return this.skills;
-//   };
-// export const ProfileM = mongoose.model<ProfileDocument>("Profile", profileSchema);
 
 export const ProfileM = mongoose.model("Profile", profileSchema);
 /* I am not adding types to this model as there  aren't any instance methods I need to define
--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,10 +1,3 @@
-// export interface IProfile {
-//   skills: string[];
-//   description: string;
-//   hourly_rate: number;
-//   languages?: string[];
-// }
-
 export interface ProfileInput {
   skills: string[];
   description: string;
